58a53c37155660223d815ae16f45832a72a6763a
gabriel8programmer <gabriel8github@gmail.com>
2023-04-25 18:15:49 -0300

    end of mini project

--- js/script.js
+++ js/script.js
@@ -6,30 +6,62 @@ const btn_prev = document.querySelector(".prev");
 const images = [...document.querySelectorAll(".image")];
 const container_images = document.querySelector(".container_images");
 
-let list_images = [];
-let image_today = 0;
 const total_size_images = images.length;
+const width_image = images[0].offsetWidth;
+let image_today = 0;
 
-//functions 
-function reset_list_images() {
-  //reset elements list images
-  list_images.map(image => {
-    container_images.appendChild(image);
-  });
-  //clear list images
-  list_images = [];
+//functions
+function start() {
+  image_today = 0;
+  //calling function update_buttons_carousel
+  update_buttons_carousel();
 }
 
-btn_next.addEventListener("click", (e) => {
+function update_carousel(index) {
+  container_images.scrollLeft = index * width_image;
+}
 
-  list_images.push(images[0]);
-  images[image_today].remove();
-
-  image_today++;
-
-  if (image_today >= total_size_images) {
-    image_today = 0;
-    reset_list_images();
+function hide_prev() {
+  if (image_today <= 0) {
+    btn_prev.style.display = "none";
+  } else {
+    btn_prev.style.display = "block";
   }
+}
 
-})
+function hide_next() {
+  if (image_today >= total_size_images - 1) {
+    btn_next.style.display = "none";
+  } else {
+    btn_next.style.display = "block";
+  }
+}
+
+function update_buttons_carousel() {
+  hide_prev();
+  hide_next();
+}
+
+//events
+
+//logic for btn prev
+btn_prev.addEventListener("click", (e) => {
+  image_today--;
+  if (image_today <= 0) {
+    image_today = 0;
+  }
+  update_carousel(image_today);
+  update_buttons_carousel();
+});
+
+//logic for btn next
+btn_next.addEventListener("click", (e) => {
+  image_today++;
+  if (image_today >= total_size_images - 1) {
+    image_today = total_size_images - 1;
+  }
+  update_carousel(image_today);
+  update_buttons_carousel();
+});
+
+start();
